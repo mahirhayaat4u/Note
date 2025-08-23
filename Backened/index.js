06d2b6af35654d827dc1dc346f4c884ff1b1eeea
Mahir Hayaat <mahirhayat4u@gmail.com>
2025-08-24 00:16:47 +0530

    frontend url added

--- Backened/index.js
+++ Backened/index.js
@@ -4,8 +4,8 @@ const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
 const app = express();
-// const port = 3000;
- const port = process.env.PORT || 3000;
+const port = 3000;
+//  const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({

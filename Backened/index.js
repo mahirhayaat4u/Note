3600c3152de4ed33f1736d0d5807109eba6a54f7
Mahir Hayaat <mahirhayat4u@gmail.com>
2025-08-30 01:05:04 +0530

    Configure project for Vercel deployment

--- Backened/index.js
+++ Backened/index.js
@@ -245,7 +245,8 @@ const port = 3000;
 // A list of URLs that are allowed to make requests to your backend
 const allowedOrigins = [
   'https://note-sage-gamma.vercel.app', // Your production URL
-  'http://localhost:5173'              // Your local development URL
+  'http://localhost:5173'  ,
+  'https://note-fesy.vercel.app/'            // Your local development URL
 ];
 
 app.use(cors({
